ca41b1701bf708de7b4dfafca70bf4f3dfa64f2e
Kunal-Shaw-097 <decodednature097@gmail.com>
2024-01-09 17:26:52 +0530

    fixed urls

--- ProjImgCap/urls.py
+++ ProjImgCap/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
 
+app_name = 'ProjImgCap'
+
 urlpatterns = [
     path('', views.Image_caption, name='ImageCaptioning'),
+    path('upload', views.Generate_caption, name="GenerateCaption") 
     # other paths...
 ]
