29f65ef4b8db9167e170edc13d752858d7b2cad4
Kunal-Shaw-097 <decodednature097@gmail.com>
2024-04-29 18:52:58 +0530

    txt input changes

--- PDF_QNA/views.py
+++ PDF_QNA/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.core.cache import cache
+
+from PDF_QNA.forms import Qform
+from PDF_QNA.apis import keys
 
 from langchain_community.vectorstores import MongoDBAtlasVectorSearch
 from langchain_community.embeddings import GPT4AllEmbeddings
@@ -11,16 +15,38 @@ from transformers import AutoTokenizer
 import transformers
 import torch
 
-#from apis import keys
+model_id = 'ericzzz/falcon-rw-1b-instruct-openorca'
+
+def load_pipeline():
+
+    pipeline  = cache.get('pipeline')
+
+    if not pipeline :
+        device = 'cuda' if torch.cuda.is_available() else 'cpu' 
+        #torch.cuda.empty_cache()
+        tokenizer = AutoTokenizer.from_pretrained(model_id)
+        pipeline = transformers.pipeline(
+            'text-generation',
+            model=model_id,
+            tokenizer=tokenizer,
+            torch_dtype=torch.bfloat16,
+            device_map=device,
+            )
+        cache.set('pipeline', pipeline, None)  
+
+    return pipeline
+
+
 
 
 # Create your views here.
 def pdf(request):
-    return render(request, "pdf.html")
+    form = Qform()
+    return render(request, "pdf.html", context={"form" : form})
 
 def qna(request):
     if request.method == 'POST':
-        print(request.FILES)
+        form = Qform()
         f = request.FILES['pdf_file']
         with open("temp.pdf", "wb+") as destination:
             for chunk in f.chunks():
@@ -32,61 +58,49 @@ def qna(request):
         text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0, separators=[
                                                     "\n\n", "\n", "(?<=\. )", " "], length_function=len)
         docs = text_splitter.split_documents(pdf)
-        #print('Split into ' + str(len(docs)) + ' docs')
-
-        # Step 3: Embed
-        # https://api.python.langchain.com/en/latest/embeddings/langchain.embeddings.openai.OpenAIEmbeddings.html
 
         gpt4all_embd = GPT4AllEmbeddings()
 
         # Step 4: Store
         # Initialize MongoDB python client
-        client = MongoClient(keys['MONGO_STR'], server_api=ServerApi('1'))
-        collection = client['try']['vec']
+        # client = MongoClient(keys['MONGO_STR'], server_api=ServerApi('1'))
+        # collection = client['try']['vec']
 
-        # # Reset w/out deleting the Search Index 
-        collection.delete_many({})
+        # # # Reset w/out deleting the Search Index 
+        # collection.delete_many({})
 
-        docsearch = MongoDBAtlasVectorSearch.from_documents(
-            docs, gpt4all_embd, collection=collection, index_name = "vector_index"
-        )
-
-
-    if query: 
-
-        if 'uploaded' not in st.session_state :
-            st.error('Please upload a PDF', icon="🚨")
+        # docsearch = MongoDBAtlasVectorSearch.from_documents(
+        #     docs, gpt4all_embd, collection=collection, index_name = "vector_index"
+        # )
         
-        else :
-            vector_search = MongoDBAtlasVectorSearch.from_connection_string(
-                keys['MONGO_STR'],
-                "try" + "." + "vec",
-                GPT4AllEmbeddings(),
-                index_name="vector_index",
-            )
+        # vector_search = MongoDBAtlasVectorSearch.from_connection_string(
+        #     keys['MONGO_STR'],
+        #     "try" + "." + "vec",
+        #     GPT4AllEmbeddings(),
+        #     index_name="vector_index",
+        # )
 
 
-            results = vector_search.similarity_search_with_score(
-                query=query, k=2
-            )
+        # results = vector_search.similarity_search_with_score(
+        #     query=query, k=2
+        # )
 
 
-            context = ''
+        # context = ''
 
-            for result in results:
-                context += result[0].page_content
+        # for result in results:
+        #     context += result[0].page_content
 
 
-            system_message = 'You are a helpful assistant. Give answers only if the information is present in the context, if information is not present answer with "Information is not present."'
-            prompt = f'<SYS> {system_message} <CONTEXT> {context} <INST> {query} <RESP> '
+        # system_message = 'You are a helpful assistant. Give answers only if the information is present in the context, if information is not present answer with "Information is not present."'
+        # prompt = f'<SYS> {system_message} <CONTEXT> {context} <INST> {query} <RESP> '
 
-            response = pipeline(
-            prompt, 
-            max_length=1024,
-            repetition_penalty=1.05
-            )
+        # response = pipeline(
+        # prompt, 
+        # max_length=1024,
+        # repetition_penalty=1.05
+        # )
 
-            response = response[0]['generated_text'].split("<RESP>")[-1]
+        # response = response[0]['generated_text'].split("<RESP>")[-1]
 
-            st.write(response)
-    return render(request, "pdf.html")
+    return render(request, "pdf.html", content_type={"form" : form})
