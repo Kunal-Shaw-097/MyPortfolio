b0636cc2174a2a0cf353ef3bedd7eebd4bfc2629
Kunal-Shaw-097 <decodednature097@gmail.com>
2024-05-27 10:27:42 +0530

    added multi-user pdf support to deploy

--- PDF_QNA/models.py
+++ PDF_QNA/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+
+def validate_file_extension(value):
+    import os
+    from django.core.exceptions import ValidationError
+    ext = os.path.splitext(value.name)[1]  # [0] returns path+filename
+    valid_extensions = ['.pdf']
+    if not ext.lower() in valid_extensions:
+        raise ValidationError('Unsupported file extension.')
+    
 # Create your models here.
+class PdfUpload(models.Model):
+    id = models.CharField(primary_key= True ,max_length=100)
+    pdf = models.FileField(upload_to='pdfs', validators=[validate_file_extension])
