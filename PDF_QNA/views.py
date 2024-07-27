2bbcdc52397e7bebac6d6d6e7cd9402ed8390126
Kunal-Shaw-097 <decodednature097@gmail.com>
2024-07-27 15:14:13 +0530

    use gpt4o-mini in server

--- PDF_QNA/views.py
+++ PDF_QNA/views.py
@@ -161,7 +161,7 @@ def qna(request):
         'content': prompt
         }
         response = client.chat.completions.create(
-                model="gpt-3.5-turbo-16k-0613",
+                model="gpt-4o-mini-2024-07-18",
                 messages=[message],
                 temperature=1,
                 max_tokens=256,
